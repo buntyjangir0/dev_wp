bd03b5e5fd9cfb49d584d3932b8fc2d08ff08f6a
Kamlesh Jangir <kamlesh@prolore.com>
2020-04-19 14:55:01 +0530

    download file afer submit form

--- wp-content/themes/custom_theme/functions.php
+++ wp-content/themes/custom_theme/functions.php
@@ -156,3 +156,50 @@ function custom_api_callback_function(){
 }
 
 
+//file download after submit
+
+function download(){
+    ?>
+    <script type="text/javascript">
+function SaveToDisk(fileURL, fileName) {
+    // for non-IE
+    if (!window.ActiveXObject) {
+        var save = document.createElement('a');
+        save.href = fileURL;
+        save.target = '_blank';
+        save.download = fileName || 'unknown';
+
+        var evt = new MouseEvent('click', {
+            'view': window,
+            'bubbles': true,
+            'cancelable': false
+        });
+        save.dispatchEvent(evt);
+
+        (window.URL || window.webkitURL).revokeObjectURL(save.href);
+    }
+
+    // for IE < 11
+    else if ( !! window.ActiveXObject && document.execCommand)     {
+        var _window = window.open(fileURL, '_blank');
+        _window.document.close();
+        _window.document.execCommand('SaveAs', true, fileName || fileURL)
+        _window.close();
+    }
+}
+
+
+     var downloadURL = 'http://localhost/wp/wp-content/uploads/2020/02/dlText.txt';   
+        document.addEventListener( 'wpcf7mailsent', function( event ) {
+if ( '32' == event.detail.contactFormId ) {
+
+var m = /[^/]*$/.exec(downloadURL)[0];
+SaveToDisk(downloadURL, m);
+}
+}, false );
+    </script>
+    <?php
+}
+add_action('wp_footer', "download");
+
+
